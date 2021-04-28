d2b0ebe2cc0c05f672d36b7a4390b145aaaa750b
Abdou00 <abdoulaye_diarra@outlook.fr>
2021-04-28 16:36:52 +0200

    Adding/Configure breeze package for a simple authentication & LFM for managing file

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,24 @@
 <?php
     use App\Http\Controllers\Back\AdminController;
     use Illuminate\Support\Facades\Route;
-
+    use \UniSharp\LaravelFilemanager\Lfm;
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
+|--------------------------------------------------------------------------|
+| Frontend Web Routes                                                      |
+|--------------------------------------------------------------------------|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
-
 Route::get('/', function () { return view('welcome');})->name('home');
 Route::get('/logout')->name('logout');
 
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => 'auth'], function () {
+    Lfm::routes();
+});
 /*
 |--------------------------------------------------------------------------|
-| Backend                                                                  |
+| Backend Web Routes                                                       |
 |--------------------------------------------------------------------------|
 */
 Route::prefix('admin')->group(function () {
@@ -26,3 +26,8 @@ Route::prefix('admin')->group(function () {
         Route::name('admin')->get('/', [AdminController::class, 'index']);
     });
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
